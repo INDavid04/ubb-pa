da457cf26e97c953c9daa91af16ef6f95d4a1ad9
INDavid04 <davidirimia04@gmail.com>
2024-11-01 23:54:58 +0200

    create appropiate variables for item name, the price and how many you have in stock

--- index.py
+++ index.py
@@ -1,2 +1,11 @@
-# Here is my first line of Python
-print("Hello World! It is easier to run two commands in index.html instead of downloading python so yeah, power web console")
+### Variables and datatypes
+print('Variables & Datatypes - Exercise')
+#Create appropriate Variables for Item name, the price 
+#and how many you have in stock
+name="John"
+price=14.99
+in_stock=5
+print(name + ' has ' + str(in_stock) + ' things of ' + str(price) + ' euro!')
+
+### Here is my first line of Python
+# print("Hello World! It is easier to run two commands in index.html instead of downloading python so yeah, power web console")
