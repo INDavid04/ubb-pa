3aec5a38b53e8db228b13cea53ed28b54489ff3b
INDavid04 <davidirimia04@gmail.com>
2024-11-02 12:04:59 +0200

    arithmetic operations

--- index.py
+++ index.py
@@ -1,11 +1,22 @@
+### Arithmetic operations
+a, b = 8, 2
+print ("For " + str(a) + " and " + str(b))
+print ("Addition: ", a + b)
+print ("Substraction: ", a -b)
+print ("Multiplication: ", a * b)
+print ("Division (float): ", a / b)
+print ("Division (floor): ", a // b)
+print ("Modulus: ", a % b)
+print ("Exponent: ", a ** b)
+
 ### Variables and datatypes
-print('Variables & Datatypes - Exercise')
-#Create appropriate Variables for Item name, the price 
-#and how many you have in stock
-name="John"
-price=14.99
-in_stock=5
-print(name + ' has ' + str(in_stock) + ' things of ' + str(price) + ' euro!')
+# print('Variables & Datatypes - Exercise')
+# #Create appropriate Variables for Item name, the price 
+# #and how many you have in stock
+# name="John"
+# price=14.99
+# in_stock=5
+# print(name + ' has ' + str(in_stock) + ' things of ' + str(price) + ' euro!')
 
 ### Here is my first line of Python
 # print("Hello World! It is easier to run two commands in index.html instead of downloading python so yeah, power web console")
