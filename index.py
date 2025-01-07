6ffda12a4114b17b0901b66f320f5afb589bb5f3
INDavid04 <davidirimia04@gmail.com>
2025-01-07 11:42:57 +0200

    Dictionaries Exercise v 1.0

--- index.py
+++ index.py
@@ -1,17 +1,43 @@
-# For Loops - Exercise
-# You are having a party and want to invite your friends. You want the print out invitations for each friend using for loops. The names are in two lists, names and names1. You also need to add two extra names to the list using an input box when you run the code. Printout one invitation to each friend per line. Names should be properly capitalized.
-# Example of printout: 
-# John Cleese! You are invited to the party on saturday.
-# Eric Idle! You are invited to the party on saturday.
-# Hint: You may need two for loops to solve this exercise.
-names = ["Ana", "Ioan", "Vlad"]
-names1 = ["George", "Mihai"]
-names.extend(names1)
-for i in range(2): 
-    name_added = input("Add another name: ")
-    names.append(name_added)
-for name in names:
-    print(f"{name.capitalize()}! You are invited to the party on saturday.")
+#It’s...not really an adventure game...#Ver 1.0
+#Your village is being attacked by 'a germanic tribe' and you need to run to the stores and get the right things to save your village, and probably some good looking girl or boy you want to marry. All prices in gold pieces excl. VAT... chop chop!! ze germanz are coming!
+#The code should allow you to get 1 thing from each store and each item you get should be removed from the store inventory, then do same for next store...
+# one way to buy by typing the key 'newt' in an input box...or something
+# at end you should print the 'items' you have taken..in this version you don't have to pay for stuff or add it up
+#ver 1.2 add ability to exit a store without buying and go to next by typing 'exit', and to exit if a nonexistant item is bought(typed)
+#Add purse with 1000 gold pieces and payment for the items during or at end of code and show a message about total cost and how much gold you have left
+#ver 1.4 random bug fix, ' browser compatability', refactoring code... basically being lazy ..stop scrolling TikTok/Facebook! ;-)
+#Ver 1.5 print inventory before and after purchases as one department_store of stuff(combine inventories from all stores into one...pretend Big Biz bought all the local stores, and want constant reporting for inventory management...)
+# as in all games there is a special way to do this that actually makes money and solves the problem...can you find 'them'? Do you know why? May require knowledge of actual python 'lore'
+
+#create stores
+freelancers = {'name':'freelancing Shop','brian': 70, 'black knight':20, 'biccus diccus':100, 'grim reaper':500, 'minstrel':-15}
+antiques = {'name':'Antique Shop','french castle':400, 'wooden grail':3, 'scythe':150, 'catapult':75, 'german joke':5}
+pet_shop = {'name':'Pet Shop','blue parrot':10, 'white rabbit':5, 'newt': 2}
+
+#create an empty shopping cart
+cart = {}
+#loop through stores/dicts
+for store in (freelancers, antiques, pet_shop) :
+    #inputbox  to show what you can buy...capture textstring of what was bought...make lowercase
+    buy_item = input(f"Welcome to {store['name']}! What do you want to buy: {store}").lower()
+    #update the cart
+    cart.update({buy_item:store.pop(buy_item)}) # use pop...
+print(f"You Purchased {', '.join(list(cart.keys()))}. Today it is all free. Have a nice day of mayhem!")
+
+# # For Loops - Exercise
+# # You are having a party and want to invite your friends. You want the print out invitations for each friend using for loops. The names are in two lists, names and names1. You also need to add two extra names to the list using an input box when you run the code. Printout one invitation to each friend per line. Names should be properly capitalized.
+# # Example of printout: 
+# # John Cleese! You are invited to the party on saturday.
+# # Eric Idle! You are invited to the party on saturday.
+# # Hint: You may need two for loops to solve this exercise.
+# names = ["Ana", "Ioan", "Vlad"]
+# names1 = ["George", "Mihai"]
+# names.extend(names1)
+# for i in range(2): 
+#     name_added = input("Add another name: ")
+#     names.append(name_added)
+# for name in names:
+#     print(f"{name.capitalize()}! You are invited to the party on saturday.")
 
 # # While Loops - Exercise
 # # Guess the correct number in 3 guesses. If you don’t get it right after 3 guesses you lose the game. 
